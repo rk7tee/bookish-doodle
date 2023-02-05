00481c8b5d5892416b3e578847a68acf19a35078
NiH2 <rk7tee@gmail.com>
2023-02-05 15:53:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,3 +79,22 @@ options = st.multiselect(
      ['Yellow', 'Red'])
 
 st.write('You selected:', options)
+
+# Day 12 - Check Box
+
+st.header('st.checkbox')
+
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
